6ca4f39976934b79cf005b723a3495885e72f00d
Janina Koivisto <koivistokms@gmail.com>
2023-04-03 14:22:03 +0300

    Created the project with Spring Initializr

---
+++ MusicDatabase/src/main/java/com/example/MusicDatabase/MusicDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicDatabaseApplication.class, args);
+	}
+
+}
---
+++ MusicDatabase/src/test/java/com/example/MusicDatabase/MusicDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
